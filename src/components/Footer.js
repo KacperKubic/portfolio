fac85a5f314ea018de650364081edbc5acb8437a
KacperKubic <kacper.kubic2211@gmail.com>
2023-01-09 16:38:19 +0100

    Footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import '../styles/footer.css'
 const Footer = () => {
     return ( 
         <div className='footer'>
-            <p>Website made by Kacper Kubić | Copyright <FaRegCopyright/> 2022 All rights reserverd</p>
+            <p>Website made by Kacper Kubić | Copyright <FaRegCopyright/> {new Date().getFullYear()} All rights reserverd</p>
             <div className='links'>
                 <a href='https://www.linkedin.com/in/kacper-kubic/'><FaLinkedin/></a>
                 <a href='https://github.com/kacperkubic'><FaGithub/></a>
